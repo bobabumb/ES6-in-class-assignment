a15645e8a7427cdeae8a52f57acc5f73233e9cb8
kenn <kennnguyen@usf.edu>
2025-03-27 14:59:11 -0400

    in class commit

---
+++ main.js
@@ -0,0 +1,17 @@
+import { getPosts } from './api.js';
+function displayPosts() {
+    const container = document.getElementById('postContainer');
+
+    getPosts()
+    .then(posts => {
+        posts.slice(0, 5) .forEach(post => {
+            const div = document.createElement('div');
+            div.innerHTML = `<h3>${post.name}<h3><p>${post.email}</p>`;
+            container.appendChild(div);
+        });
+    })
+    .catch(err => {
+        container.innerHTML = `<p style="color:red;">failed to load posts: ${err.message}</p>;`
+    });
+}
+displayPosts();
